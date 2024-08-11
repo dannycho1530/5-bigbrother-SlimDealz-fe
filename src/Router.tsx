b142b9a62a4f18ed35c1bfe057e1e57e12f60df8
whdmldus1116 <76983867+whdmldus1116@users.noreply.github.com>
2024-08-11 22:21:14 +0900

    feat: Router 파일 설정

---
+++ src/Router.tsx
@@ -0,0 +1,38 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import OutLetContainer from './pages';
+import MainPage from './pages/main';
+import CategoryPage from './pages/category';
+import DetailPage from './pages/detail';
+import SearchInitialPage from './pages/search/initial';
+import SearchResultsPage from './pages/search/results';
+import SignInPage from './pages/signIn';
+import SignUpPage from './pages/signUp';
+import UserAlarmPage from './pages/user/alarm';
+import UserBookmarkPage from './pages/user/bookmark';
+import UserInformationPage from './pages/user/information';
+import UserRecentlyViewPage from './pages/user/recentlyView';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/main" />} />
+        <Route path="/" element={<OutLetContainer />}>
+          <Route path="main" element={<MainPage />} />
+          <Route path="category" element={<CategoryPage />} />
+          <Route path="detail" element={<DetailPage />} />
+          <Route path="searchInitial" element={<SearchInitialPage />} />
+          <Route path="searchResults" element={<SearchResultsPage />} />
+          <Route path="signIn" element={<SignInPage />} />
+          <Route path="signUp" element={<SignUpPage />} />
+          <Route path="alarm" element={<UserAlarmPage />} />
+          <Route path="bookmark" element={<UserBookmarkPage />} />
+          <Route path="information" element={<UserInformationPage />} />
+          <Route path="recentlyView" element={<UserRecentlyViewPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
