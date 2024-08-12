a5be7a457c3eba68320923deef2e69fbca535e54
rhea <dev.rhea4444@gmail.com>
2024-08-12 21:06:55 +0900

    Feat: 페이지 이름 생성

---
+++ src/components/utils/pageName/index.tsx
@@ -0,0 +1,15 @@
+import { PageNameContainer } from './styles';
+
+type Props = {
+  pageName: string;
+};
+
+const PageNameTag = ({ pageName }: Props) => {
+  return (
+    <PageNameContainer>
+      <div>{pageName}</div>
+    </PageNameContainer>
+  );
+};
+
+export default PageNameTag;
---
+++ src/components/utils/pageName/styles.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const PageNameContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: auto;
+  font-size: 15px;
+`;
