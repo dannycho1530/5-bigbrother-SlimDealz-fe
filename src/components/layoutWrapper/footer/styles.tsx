971614e259f19d59935dd53f2bf133e85875c4a6
rhea <dev.rhea4444@gmail.com>
2024-08-12 20:06:19 +0900

    Feat: 푸터 생성

---
+++ src/components/layoutWrapper/footer/styles.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const FooterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 35px;
+  width: 390px;
+  height: 70px;
+`;
