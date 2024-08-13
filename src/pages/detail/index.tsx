aeae3bce7fd2c5839fe1c303604cc16840d5d5d2
whdmldus1116 <76983867+whdmldus1116@users.noreply.github.com>
2024-08-13 15:53:01 +0900

    feat: 차트, 최저가 비교 목업으로 구성

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from './styles';
 import { useParams } from 'react-router-dom';
 import PriceAlertSetting from '../../components/priceAlertSetting';
+import TabsComponent from '../../components/tab';
 
 const DetailPage = () => {
   const { productId } = useParams<{ productId: string }>();
@@ -9,6 +10,7 @@ const DetailPage = () => {
   return (
     <Container>
       <PriceAlertSetting />
+      <TabsComponent />
     </Container>
   );
 };
