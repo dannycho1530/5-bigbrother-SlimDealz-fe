a16755a257728da4c28455acb3c404bcfe088ea4
whdmldus1116 <76983867+whdmldus1116@users.noreply.github.com>
2024-08-19 14:03:23 +0900

    chore: @emotion 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-//
-const path = require("path");
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+      '@': path.resolve(__dirname, './src'),
+      '@mui/styled-engine': '@mui/styled-engine-sc' // 추가된 코드
+    }
   },
   plugins: [react()],
   build: {
-    outDir: "dist",
+    outDir: 'dist'
   },
   server: {
-    port: 3000,
-  },
+    port: 3000
+  }
 });
