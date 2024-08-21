5ac250fb66968f6bbbd7b48b4fe73d28a9958086
whdmldus1116 <76983867+whdmldus1116@users.noreply.github.com>
2024-08-21 10:15:10 +0900

    feat: 글씨체 로드 지연 해결

--- src/styles/globalStyles.tsx
+++ src/styles/globalStyles.tsx
@@ -1,99 +1,23 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-  @font-face {
-    font-family: 'Oswald';
-    src: url('https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600;700&display=swap');
-    font-weight: normal;
-    font-style: normal;
-  }
   @import url(//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css);
   
   :root {
     --background-color: white;
   }
 
-  html,
-  body,
-  div,
-  span,
-  applet,
-  object,
-  iframe,
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  p,
-  blockquote,
-  pre,
-  a,
-  abbr,
-  acronym,
-  address,
-  big,
-  cite,
-  code,
-  del,
-  dfn,
-  em,
-  img,
-  ins,
-  kbd,
-  q,
-  s,
-  samp,
-  small,
-  strike,
-  strong,
-  sub,
-  sup,
-  tt,
-  var,
-  b,
-  u,
-  i,
-  center,
-  dl,
-  dt,
-  dd,
-  ol,
-  ul,
-  li,
-  fieldset,
-  form,
-  label,
-  legend,
-  table,
-  caption,
-  tbody,
-  tfoot,
-  thead,
-  tr,
-  th,
-  td,
-  article,
-  aside,
-  canvas,
-  details,
-  embed,
-  figure,
-  figcaption,
-  footer,
-  header,
-  hgroup,
-  menu,
-  nav,
-  output,
-  ruby,
-  section,
-  summary,
-  time,
-  mark,
-  audio,
-  video {
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center, dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend, table, caption,
+  tbody, tfoot, thead, tr, th, td, article, aside,
+  canvas, details, embed, figure, figcaption, footer,
+  header, hgroup, menu, nav, output, ruby, section,
+  summary, time, mark, audio, video {
     margin: 0;
     padding: 0;
     border: 0;
@@ -103,43 +27,26 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  article,
-  aside,
-  details,
-  figcaption,
-  figure,
-  footer,
-  header,
-  hgroup,
-  menu,
-  nav,
-  section {
+  article, aside, details, figcaption, figure,
+  footer, header, hgroup, menu, nav, section {
     display: block;
   }
 
   body {
     line-height: 1;
-    font-family: 'Spoqa Han Sans Neo', sans-serif;
+    font-family: 'Spoqa Han Sans Neo','Oswald', sans-serif;
   }
 
-  ol,
-  ul {
+  ol, ul {
     list-style: none;
   }
 
-  blockquote,
-  q {
+  blockquote, q {
     quotes: none;
   }
 
-  blockquote:before,
-  blockquote:after {
-    content: '';
-    content: none;
-  }
-
-  q:before,
-  q:after {
+  blockquote:before, blockquote:after,
+  q:before, q:after {
     content: '';
     content: none;
   }
